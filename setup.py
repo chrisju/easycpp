367f1191bc9da12e868abc3ea3416c213fe52e05
chrisju <mchrisju@gmail.com>
2025-03-19 18:17:30 +0900

    BSD -> MIT

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# TODO README
+# TODO translate
 setup(
     name="easycpp",  # 模块的名称
     version="1.0.0",  # 版本号
@@ -11,8 +13,8 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Chris Ju",  # 作者信息
-    author_email="zhuzhu101011@gmail.com",  # 邮箱
-    url="http://blog2.moez.win/",  # 项目主页（可选）
+    author_email="mchrisju@gmail.com",  # 邮箱
+    url="https://github.com/chrisju/easycpp",  # 项目主页（可选）
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",  # 或其他许可证
